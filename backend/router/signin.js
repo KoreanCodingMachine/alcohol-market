2daa5099d8d4d6c5547e24b589e260b8583f1274
sherwooz <sherwooz@sherwoozs-MacBook-Air.local>
2023-07-16 17:12:43 +0900

    유저 모델

--- backend/router/signin.js
+++ backend/router/signin.js
@@ -1,34 +1,39 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-import { getDBInstance } from '../db.js';
+import User from '../models/user.js';
 
-const privateKey = 'SOOL_DAM_HWA'
+const privateKey = 'SOOL_DAM_HWA';
 
 export default async function (ctx, next) {
-    const db = getDBInstance();
-    const { username, password } = ctx.request.body;
-    const usersCollection = db.collection('users');
-  
-    const user = await usersCollection.findOne({ username });
-  
+  const { username, password } = ctx.request.body;
+
+  try {
+    const user = await User.findOne({ username:  username});
+
+    console.log("user", user, username, password)
     if (!user) {
       ctx.status = 400;
       ctx.body = { message: 'Invalid username or password' };
       return;
     }
-  
+
     const isValidPassword = await bcrypt.compare(password, user.password);
+
     if (!isValidPassword) {
       ctx.status = 400;
       ctx.body = { message: 'Invalid username or password' };
       return;
     }
-  
+
     const token = jwt.sign({ username: user.username }, privateKey, {
       expiresIn: '1h',
     });
-  
+
     ctx.status = 200;
     ctx.body = { message: 'Logged in successfully', token };
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { message: 'Unexpected error occurred.' };
+  }
 }
--- backend/router/signup.mjs
+++ backend/router/signup.mjs
@@ -1,24 +1,27 @@
-import bcrypt from 'bcrypt'
-import { getDBInstance } from '../db.js';
+import bcrypt from 'bcrypt';
+import User from '../models/user.js';
 
 export default async function (ctx, next) {
-  const db = getDBInstance();
-    const { username, password } = ctx.request.body;
-    const usersCollection = db.collection('users');
-    
-    const userExists = await usersCollection.findOne({ username });
-  
+  const { username, password } = ctx.request.body;
+
+  try {
+    const userExists = await User.findOne({ username });
+
     if (userExists) {
       ctx.status = 400;
       ctx.body = { message: 'User already exists' };
       return;
     }
-  
+
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = { username, password: hashedPassword };
-    
-    await usersCollection.insertOne(newUser);
-  
+    const newUser = new User({ username, password: hashedPassword });
+
+    await newUser.save();
+
     ctx.status = 201;
     ctx.body = { message: 'User created successfully', username };
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { message: 'Unexpected error occurred.' };
+  }
 }
