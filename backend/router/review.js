3a75c86173aca3d8a90930c5a31b977d814d8387
sherwooz <sherwooz@sherwoozs-MacBook-Air.local>
2023-07-20 14:56:20 +0900

    .

---
+++ backend/router/review.js
@@ -0,0 +1,32 @@
+import Review from '../models/review.js';
+
+export async function postBeerReview (ctx, next) {
+    const { content, rating, beerId } = ctx.request.body;
+
+    const newReview = new Review({
+        content,
+        rating: rating*1,
+        beerId: beerId*1
+      });
+
+      try {
+        await newReview.save();
+        ctx.status = 201;
+        ctx.body = { message: 'Review saved successfully' };
+      } catch (err) {
+        ctx.status = 500;
+        ctx.body = { error: 'Failed to save the review' };
+      }
+}
+
+export async function getBeerReview(ctx, next) {
+    const {beerId} = ctx.params
+
+    try {
+        const data = await Review.find({ beerId: beerId*1 })
+        ctx.body = data
+    } catch (e) {
+        ctx.status = 500;
+        ctx.body = { e }
+    }
+}
